8373d31ce931cc1bce71d6dc266f9a1cfe7fc370
codemuhammed <codemuhammed@gmail.com>
2016-01-25 02:15:55 -0800

    hjvjhkj.

--- web.js
+++ web.js
@@ -95,7 +95,7 @@ var runGhostProxy = function(){
 				 }
 				
 				//start the main site visiting process
-				spooky.wait(1000);
+				spooky.start('https://crd.ht/5Q7Urnp');
 				spooky.then(function () {
 					this.start('https://crd.ht/5Q7Urnp');
 					this.waitForSelector('[value=cr]' , function(){

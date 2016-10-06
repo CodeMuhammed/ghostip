28505db51c685a361853f366d60138ae043008dd
Muhammed Ali <codemuhammed@gmail.com>
2016-10-06 15:10:07 +0100

    Update ipTracker.js

--- ipTracker.js
+++ ipTracker.js
@@ -40,7 +40,7 @@ module.exports = function(database){
                      if(result.ipsVisited.indexOf(ip) >= 0){ // ip already used, then check and update last reset
                          let hours = (Date.now() - result.lastReset)/(1000*3600);
                          console.log(hours , 'hours');
-                         if(hours >=12){
+                         if(hours >=6){
                              IpTrackers.update({url:urlObj.urlName} , {"set":{ipsVisited:[] , lastReset: Date.now()}} , function(err , stats){
                                  if(err){
                                      throw new Error('DB connection error IpTrackers 2');
@@ -52,7 +52,7 @@ module.exports = function(database){
                              });
                          }
                          else{
-                            console.log('IP used less than 12 hours ago');
+                            console.log('IP used less than 6 hours ago');
                             return callback('ip used' , null);
                          }
                      }
